8079acfc87563c82c18b8145b674fdd1c6d8aaff
Je-ric <164357842+Je-ric@users.noreply.github.com>
2025-04-04 23:13:21 +0800

    clean

--- PHP-Projects/Student-Portal-with-Login-OOP/session_config.php
+++ PHP-Projects/Student-Portal-with-Login-OOP/session_config.php
@@ -1,4 +1,4 @@
-<?php
-ini_set('session.cookie_lifetime', 30 * 24 * 60 * 60);
-ini_set('session.gc_maxlifetime', 30 * 24 * 60 * 60);
+<?php 
+    ini_set('session.cookie_lifetime', 30 * 24 * 60 * 60); // optional lang parehas
+    ini_set('session.gc_maxlifetime', 30 * 24 * 60 * 60);
 ?>
